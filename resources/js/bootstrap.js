3284e52e078b5c4a5802b11c0287634faceb04e1
Tarık Ünal <47159785+mrtarikunal@users.noreply.github.com>
2020-05-13 05:17:04 +0300

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,7 +36,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  window.Echo = new Echo({
      broadcaster: 'pusher',
      //key: process.env.MIX_PUSHER_APP_KEY,
-     key: "4dd649e266d1e7d1e183",
+     key: "your key",
      //cluster: process.env.MIX_PUSHER_APP_CLUSTER,
      cluster: "eu",
      encrypted: true
